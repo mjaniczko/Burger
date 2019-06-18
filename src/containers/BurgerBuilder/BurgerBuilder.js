033e8623e19b8ae8e46970a08bf8ba8362d5691b
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-18 21:33:03 +0200

    Burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-  render () {
+  render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
-    )
+    );
   }
 }
 
-export default BurgerBuilder
+export default BurgerBuilder;

4e4ac845b322261922e19a7f59cecb0188875a8f
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-19 17:09:32 +0200

    Add message if no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,26 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      {transformedIngredients}
+      {transformedIngredients.length === 0 ? (
+        <p>Please start adding ingredients!</p>
+      ) : (
+        transformedIngredients
+      )}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );

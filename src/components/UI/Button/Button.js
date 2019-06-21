977d300736db71ea8446b46130a15e8fd59ba011
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-21 20:11:57 +0200

    Add custom button

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Button.css';
+
+const button = props => (
+  <button
+    onClick={props.clicked}
+    className={[classes.Button, classes[props.btnType]].join(' ')}
+  >
+    {props.children}
+  </button>
+);
+
+export default button;

0aac7e0181105f8f6e90ac82519725ae2a2cdbf1
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-22 21:55:33 +0200

    Create toolbar skeleton

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => (
   <Aux>
-    <div>
-      Toolbar, SideDrawer, Backdrop
-      <main className={classes.Content}>{props.children}</main>
-    </div>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 

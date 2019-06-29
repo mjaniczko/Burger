b3da6d62376983598fd67882f22f56fe4b30c909
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-29 22:59:20 +0200

    Building checkout page burger from URL params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,13 +4,19 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
-    }
+    ingredients: {}
   };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+
+    this.setState({ ingredients: ingredients });
+  }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();

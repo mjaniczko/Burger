bb3b0e9999537caab63194850239e75734ff5bf9
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-30 18:57:46 +0200

    Order component - not dynamic yet

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = props => (
+  <div className={classes.Order}>
+    <p>Ingredients: </p>
+    <p>
+      Price: <strong />
+    </p>
+  </div>
+);
+
+export default order;

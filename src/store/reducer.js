4bea86e91110f946a4b2caa77412212043c806ad
Michał Janiczko <michaljaniczko1@gmail.com>
2019-07-04 19:18:23 +0200

    Add payload

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -17,7 +17,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         ingredients: {
           ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+          [action.payload.ingredientName]:
+            state.ingredients[action.payload.ingredientName] + 1
         }
       };
     case actionTypes.REMOVE_INGREDIENT:
@@ -25,7 +26,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         ingredients: {
           ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+          [action.payload.ingredientName]:
+            state.ingredients[action.payload.ingredientName] - 1
         }
       };
     default:

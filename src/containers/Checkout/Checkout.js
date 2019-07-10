86b09b22c93ffdcee9f8b2fd871018e63768cdba
Michał Janiczko <michaljaniczko1@gmail.com>
2019-07-10 22:11:37 +0200

    Handle redirect after order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -16,9 +16,14 @@ class Checkout extends Component {
 
   render() {
     let summary = <Redirect to='/' />;
+
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to='/' />
+      ) : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -37,7 +42,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 

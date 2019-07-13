6b2806b1678386fc4c6a6a55f7b8919eed82d761
Michał Janiczko <michaljaniczko1@gmail.com>
2019-07-13 18:15:16 +0200

    New actions fetch related

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -36,9 +36,45 @@ export const purchaseBurger = orderData => {
   };
 };
 
-
 export const purchaseInit = () => {
   return {
     type: actionTypes.PURCHASE_INIT
-  }
-}
+  };
+};
+
+export const fetchOrdersSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = error => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    axios
+      .get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFail(err));
+      });
+  };
+};

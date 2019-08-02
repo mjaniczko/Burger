e4d12309d3c850ecd97235908e5a7771da27decf
Michał Janiczko <michaljaniczko1@gmail.com>
2019-08-02 16:09:28 +0200

    Logout reducer

--- src/store/reducer/auth.js
+++ src/store/reducer/auth.js
@@ -26,10 +26,15 @@ const authFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 };
 
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null })
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
     default: return state;
   }

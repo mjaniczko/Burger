a9fb6793c6970a3d7d477dae3048ae880384ed70
Michał Janiczko <michaljaniczko1@gmail.com>
2019-08-05 21:45:16 +0200

    remove console.log

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,11 +6,10 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-  }
-
-  componentWillUpdate() {
-    console.log('[Modal]: WillUpdate');
+    return (
+      nextProps.show !== this.props.show ||
+      nextProps.children !== this.props.children
+    );
   }
 
   render() {
